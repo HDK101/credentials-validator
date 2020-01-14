f75c1f467d57ca4bdd2fd7a5033a292fdffebad2
Lucas Hideki <lucashideki87@gmail.com>
2020-01-14 14:55:30 -0300

    Validator now checks for uppercase, numbers and special chars in password

--- src/validator.js
+++ src/validator.js
@@ -4,6 +4,10 @@ var settings = {
   passwordMin: 8,
   passwordMax: 20,
   passwordCharMustContain: [],
+  passwordMustContainUpper: false,
+  passwordMustContainNumber: false,
+  passwordMustContainSpace: false,
+  passwordSpecialCharacters: "forbidden",
   emailMustContain: ["@", "."]
 };
 
@@ -20,7 +24,7 @@ function useDefaultSettings() {
 
 function setSettings(set) {
   settings = Object.assign(settings, set);
-  console.log(settings);
+  console.table(settings);
 }
 
 function validate(user, callback) {
@@ -31,12 +35,15 @@ function validate(user, callback) {
     nameMax,
     passwordMin,
     passwordMax,
-    emailMustContain
+    passwordMustContainUpper,
+    passwordMustContainNumber,
+    passwordSpecialCharacters
   } = settings;
   const passwordCharMustContain = settings.passwordCharMustContain.join();
 
   var errors = [];
   if (name !== undefined) {
+    /*Verifying name length*/
     const nameMinError =
       name.length >= nameMin
         ? ""
@@ -46,7 +53,7 @@ function validate(user, callback) {
         ? ""
         : `Name length should be equal or less than ${nameMax}`;
     errors.push(nameMinError, nameMaxError);
-  } else if (name !== undefined & name == "") {
+  } else if ((name !== undefined) & (name == "")) {
     errors.push("Empty name!");
   }
   if (email !== undefined) {
@@ -58,18 +65,11 @@ function validate(user, callback) {
 
     /*Verify if is an valid email provider*/
     if (!emailProvider[0]) errors.push("Invalid email!");
-
-    /*Verify if email has the necessary characters*/
-    // for (var index = 0; index < emailMustContain.length; index++) {
-    //   if (!email.includes(emailMustContain[index])) {
-    //     errors.push("Invalid email!");
-    //     break;
-    //   }
-    // }
-  } else if (email !== undefined & email == "") {
+  } else if ((email !== undefined) & (email == "")) {
     errors.push("Empty email!");
   }
   if (password !== undefined) {
+    /*Verifying password length*/
     const passwordMinError =
       password.length >= passwordMin
         ? ""
@@ -81,13 +81,42 @@ function validate(user, callback) {
 
     errors.push(passwordMinError, passwordMaxError);
 
+    /*Verifying if password contains certain characters*/
     if (passwordCharMustContain) {
       const errorText = "Password should contain this character: ";
       passwordCharMustContain.forEach(element => {
         if (!password.includes(element)) errors.push(`${errorText}${element}`);
       });
     }
-  } else if (password !== undefined & password == "") {
+
+    /*Verify if password contains uppercase*/
+    regexUpper = /[A-Z]/;
+    if (passwordMustContainUpper) {
+      regexUpper.test(password)
+        ? console.log("Password contains uppercase!")
+        : errors.push("Password should contain at last 1 uppercase");
+    }
+
+    /*Verify if password contains number*/
+    regexNumber = /[0-9]/;
+    if (passwordMustContainNumber) {
+      regexNumber.test(password)
+        ? console.log("Password contains number!")
+        : errors.push("Password should contain at last 1 number");
+    }
+
+    /*Verify if password*/
+    regexSpecial = /^[A-Za-z0-9 ]+$/;
+    if (passwordSpecialCharacters == "forbidden") {
+      regexSpecial.test(password)
+        ? console.log("Password doesn't contains special characters!")
+        : errors.push("Forbidden characters in password");
+    } else if (passwordSpecialCharacters == "permitted") {
+      regexSpecial.test(password)
+        ? errors.push("Password doesn't contains special characters!")
+        : console.log("Password contains special characters!");
+    }
+  } else if ((password !== undefined) & (password == "")) {
     errors.push("Empty password!");
   }
   if (errors) {
@@ -105,4 +134,4 @@ function validate(user, callback) {
   }
 }
 
-module.exports = { useDefaultSettings, setSettings, validate };
+module.exports = { settings, useDefaultSettings, setSettings, validate };

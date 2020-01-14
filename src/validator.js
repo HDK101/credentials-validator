d761ccfdfbe47b8cef54626420a8f153fd0fce96
Lucas Hideki <lucashideki87@gmail.com>
2020-01-14 15:21:08 -0300

    Validate() was divided by three functions

--- src/validator.js
+++ src/validator.js
@@ -42,83 +42,17 @@ function validate(user, callback) {
   const passwordCharMustContain = settings.passwordCharMustContain.join();
 
   var errors = [];
-  if (name !== undefined) {
-    /*Verifying name length*/
-    const nameMinError =
-      name.length >= nameMin
-        ? ""
-        : `Name length should be equal or higher than ${nameMin}`;
-    const nameMaxError =
-      name.length <= nameMax
-        ? ""
-        : `Name length should be equal or less than ${nameMax}`;
-    errors.push(nameMinError, nameMaxError);
-  } else if ((name !== undefined) & (name == "")) {
-    errors.push("Empty name!");
-  }
-  if (email !== undefined) {
-    const emailParts = email.split("@");
-    const emailProvider = emailParts.length > 1 ? emailParts[1].split(".") : [];
+  checkName(name, function(error) {
+    errors.push(error);
+  });
 
-    /*Verify if exists a value before @*/
-    if (!emailParts[0]) errors.push("Invalid email!");
+  checkEmail(email, function(error) {
+    errors.push(error);
+  });
 
-    /*Verify if is an valid email provider*/
-    if (!emailProvider[0]) errors.push("Invalid email!");
-  } else if ((email !== undefined) & (email == "")) {
-    errors.push("Empty email!");
-  }
-  if (password !== undefined) {
-    /*Verifying password length*/
-    const passwordMinError =
-      password.length >= passwordMin
-        ? ""
-        : `Password length should be equal or higher than ${passwordMin}`;
-    const passwordMaxError =
-      password.length <= passwordMax
-        ? ""
-        : `Password length should be equal or less than ${passwordMax}`;
-
-    errors.push(passwordMinError, passwordMaxError);
-
-    /*Verifying if password contains certain characters*/
-    if (passwordCharMustContain) {
-      const errorText = "Password should contain this character: ";
-      passwordCharMustContain.forEach(element => {
-        if (!password.includes(element)) errors.push(`${errorText}${element}`);
-      });
-    }
-
-    /*Verify if password contains uppercase*/
-    regexUpper = /[A-Z]/;
-    if (passwordMustContainUpper) {
-      regexUpper.test(password)
-        ? console.log("Password contains uppercase!")
-        : errors.push("Password should contain at last 1 uppercase");
-    }
-
-    /*Verify if password contains number*/
-    regexNumber = /[0-9]/;
-    if (passwordMustContainNumber) {
-      regexNumber.test(password)
-        ? console.log("Password contains number!")
-        : errors.push("Password should contain at last 1 number");
-    }
-
-    /*Verify if password*/
-    regexSpecial = /^[A-Za-z0-9 ]+$/;
-    if (passwordSpecialCharacters == "forbidden") {
-      regexSpecial.test(password)
-        ? console.log("Password doesn't contains special characters!")
-        : errors.push("Forbidden characters in password");
-    } else if (passwordSpecialCharacters == "permitted") {
-      regexSpecial.test(password)
-        ? errors.push("Password doesn't contains special characters!")
-        : console.log("Password contains special characters!");
-    }
-  } else if ((password !== undefined) & (password == "")) {
-    errors.push("Empty password!");
-  }
+  CheckPassword(password, function(error) {
+    errors.push(error);
+  });
   if (errors) {
     /*Clear duplicate or empty strings*/
     var cleanedErrors = [];
@@ -131,6 +65,87 @@ function validate(user, callback) {
 
     /*Pass errors to callback*/
     callback(cleanedErrors);
+  }
+}
+
+function checkName(name, callback) {
+  if (name !== undefined) {
+    /*Verifying name length*/
+    name.length >= nameMin
+      ? console.log("Name length is equal or higher than min!")
+      : callback(`Name length should be equal or higher than ${nameMin}`);
+    name.length <= nameMax
+      ? console.log("Name length is equal or less than max!")
+      : callback(`Name length should be equal or less than ${nameMax}`);
+  } else if ((name !== undefined) & (name == "")) {
+    return callback(`Name length should be equal or higher than ${nameMin}`);
+  }
+}
+
+function checkEmail(email, callback) {
+  if (email !== undefined) {
+    const emailParts = email.split("@");
+    const emailProvider = emailParts.length > 1 ? emailParts[1].split(".") : [];
+
+    /*Verify if exists a value before @*/
+    if (!emailParts[0]) return callback("Invalid email!");
+
+    /*Verify if is an valid email provider*/
+    if (!emailProvider[0]) return callback("Invalid email!");
+  } else if ((email !== undefined) & (email == "")) {
+    return callback("Empty email!");
+  }
+}
+
+function CheckPassword(password, callback) {
+  if (password !== undefined) {
+    /*Verifying password length*/
+    password.length >= passwordMin
+      ? ""
+      : callback(
+          `Password length should be equal or higher than ${passwordMin}`
+        );
+    password.length <= passwordMax
+      ? ""
+      : callback(`Password length should be equal or less than ${passwordMax}`);
+
+    /*Verifying if password contains certain characters*/
+    if (passwordCharMustContain) {
+      const errorText = "Password should contain this character: ";
+      passwordCharMustContain.forEach(element => {
+        if (!password.includes(element))  callback(`${errorText}${element}`);
+      });
+    }
+
+    /*Verify if password contains uppercase*/
+    regexUpper = /[A-Z]/;
+    if (passwordMustContainUpper) {
+      regexUpper.test(password)
+        ? console.log("Password contains uppercase!")
+        : callback("Password should contain at last 1 uppercase");
+    }
+
+    /*Verify if password contains number*/
+    regexNumber = /[0-9]/;
+    if (passwordMustContainNumber) {
+      regexNumber.test(password)
+        ? console.log("Password contains number!")
+        : callback("Password should contain at last 1 number");
+    }
+
+    /*Verify if password*/
+    regexSpecial = /^[A-Za-z0-9 ]+$/;
+    if (passwordSpecialCharacters == "forbidden") {
+      regexSpecial.test(password)
+        ? console.log("Password doesn't contains special characters!")
+        : callback("Forbidden characters in password");
+    } else if (passwordSpecialCharacters == "permitted") {
+      regexSpecial.test(password)
+        ? errors.push("Password doesn't contains special characters!")
+        : callback("Password contains special characters!");
+    }
+  } else if ((password !== undefined) & (password == "")) {
+    return callback("Empty password!");
   }
 }
 

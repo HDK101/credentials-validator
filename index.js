c971308699ac05cf251c814e281b51f0d3318977
Lucas Hideki <lucashideki87@gmail.com>
2020-01-14 20:45:29 -0300

    Applied prettier to index

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const validator = require("./src/validator");
 
 user = {
-    name: "AnnieCarr1010",
-    email: "annie@email.com",
-    password: "Password10"
+  name: "AnnieCarr1010",
+  email: "annie@email.com",
+  password: "Password10"
 };
 
 settings = {
-    passwordMustContainUpper: true,
-    passwordMustContainNumber: true
-}
+  passwordMustContainUpper: true,
+  passwordMustContainNumber: true
+};
 
 validator.setSettings(settings);
 
 validator.validate(user, function(errors) {
-    console.table(errors);
-})
+  console.table(errors);
+});
